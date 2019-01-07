1a1621e537f21375e74e9d8e82de3a400e58f998
Anderson Madeira <andersonmadeiracs@gmail.com>
2019-01-06 22:28:54 -0300

    Fixes #13

--- src/TaskListItem.js
+++ src/TaskListItem.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { TouchableHighlight, View, Text, TouchableOpacity } from 'react-native';
+import { TouchableHighlight, View, Text, TouchableOpacity, StyleSheet } from 'react-native';
 import CheckBox from './CheckBox';
 import TaskService from './TaskService';
 import RemoveButton from './RemoveButton';
@@ -48,13 +48,36 @@ export default class TaskListItem extends Component {
     let textDecorationLine = task.completed ? 'line-through' : 'none';
 
     return (
-        <Animatable.View ref={this.handleAnimatedViewRef} animation="fadeIn" style={{ flexDirection: 'row', alignItems: 'center', paddingTop: 6, paddingBottom: 6, backgroundColor: "#F8F8F8", borderBottomWidth: 1, borderColor: '#eee' }}>
-          <CheckBox task={task} color={color} onCheckBoxPressed={this._onCheckBoxPressed}></CheckBox>
-          <Text style={{ fontSize: 18, color: color, textDecorationLine: textDecorationLine }}>{task.title}</Text>
-          <View style={{ flex: 1, flexDirection: 'row', justifyContent: 'flex-end' }}>
-            <RemoveButton onRemovePressed={this._onRemovePressed}/>
-          </View>
-        </Animatable.View>
+      <Animatable.View ref={this.handleAnimatedViewRef} animation="fadeIn" style={styles.container}>
+        <CheckBox task={task} color={color} onCheckBoxPressed={this._onCheckBoxPressed} style={styles.checkBox}></CheckBox>
+        <Text style={[styles.text, { color: color, textDecorationLine: textDecorationLine }]}>{task.title}</Text>
+        <View style={styles.rightButtons}>
+          <RemoveButton onRemovePressed={this._onRemovePressed} />
+        </View>
+      </Animatable.View>
     )
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    paddingTop: 6,
+    paddingBottom: 6,
+    backgroundColor: "#F8F8F8",
+    borderBottomWidth: 1,
+    borderColor: '#eee',
+  },
+  checkBox: {
+  },
+  text: {
+    flex: 1,
+    fontSize: 18,
+    paddingRight: 5,
+  },
+  rightButtons: {
+    flexDirection: 'row',
+    justifyContent: 'flex-end'
+  }
+})
